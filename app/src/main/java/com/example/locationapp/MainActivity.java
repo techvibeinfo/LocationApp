be1734cd6d371dcfe726d2c55c4d3c37f05b64db
Vaisakh K P <vaisakh.kp@cherrylabs.com>
2020-02-20 14:14:20 +0530

    first commit

---
+++ app/src/main/java/com/example/locationapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.locationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button routeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        routeButton = findViewById(R.id.routeButton);
+
+
+        routeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DirectionActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
